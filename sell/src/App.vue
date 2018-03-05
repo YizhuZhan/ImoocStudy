587a6afa34c83bc64b92362a14cfe1c810de52f5
zhanyizhu <zhyzh910914@126.com>
2018-03-05 19:14:21 +0800

    2018.3.5 提交vue-elm代码

---
+++ sell/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class = "tab-item">商品</div>
+      <div class = "tab-item">评价</div>
+      <div class = "tab-item">商家</div>
+    </div>
+    <div class="content">
+      I am content
+    </div>
+</div>
+</template>
+
+<script>
+  import header from './components/header/header';
+  export default {
+    components: {
+      'v-header': header
+    }
+  };
+</script>
+
+<style lang="stylus" ref="stylesheet/stylus">
+  #app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height:40px
+      .tab-item
+        flex:1
+        text-align: center
+</style>
---
+++ sell/src/components/header/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class = "header">我是 header</div>
+</template>
+
+<script type="text/ecmascript6">
+  export {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
