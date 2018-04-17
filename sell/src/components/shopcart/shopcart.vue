f2b94ad1e3eb83e844179ed1965cd8e244a0cfa4
zhanyizhu <zhyzh910914@126.com>
2018-04-17 19:32:40 +0800

    2018.4.17 elm项目购物车详情

--- sell/src/components/shopcart/shopcart.vue
+++ sell/src/components/shopcart/shopcart.vue
@@ -16,44 +16,44 @@
           <div class="pay" :class="payClass">{{payDesc}}</div>
         </div>
       </div>
-    </div>
-    <div class="ball-container">
-      <div v-for="(ball, index) in balls" v-bind:key="index">
-        <transition name="drop" @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
-          <div class="ball" v-show="ball.show">
-            <div class="inner inner-hook"></div>
-          </div>
-        </transition>
+      <div class="ball-container">
+        <div v-for="(ball, index) in balls" v-bind:key="index">
+          <transition name="drop" @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+            <div class="ball" v-show="ball.show">
+              <div class="inner inner-hook"></div>
+            </div>
+          </transition>
+        </div>
       </div>
-    </div>
-    <transition name="fold">
-      <div class="shopcart-list" v-show="listShow">
-        <div class="list-header">
-          <h1 class="title">购物车</h1>
-          <span class="empty">清空</span>
+      <transition name="fold">
+        <div class="shopcart-list">
+          <div class="list-header">
+            <h1 class="title">购物车</h1>
+            <span class="empty">清空</span>
+          </div>
+          <div class="list-content">
+            <ul>
+              <li v-for="(food, index) in selectFoods" class="food" v-bind:key="index">
+                <span class="name">{{food.name}}</span>
+                <div class="price">
+                  <span>{{food.price*food.count}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="list-content">
-          <ul>
-            <li v-for="food in selectFoods" class="food">
-              <span class="name">{{food.name}}</span>
-              <div class="price">
-                <span>{{food.price*food.count}}</span>
-              </div>
-              <div class="cartcontrol-wrapper">
-                <cartcontrol :food="food"></cartcontrol>
-              </div>
-            </li>
-          </ul>
-        </div>
+      </transition>
     </div>
-    </transition>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import Cartcontrol from "../cartcontrol/cartcontrol";
+  import cartcontrol from '../cartcontrol/cartcontrol';
   export default {
-    components: {Cartcontrol},
+    components: {cartcontrol},
     props: {
       deliveryPrice: {
         type: Number,
@@ -133,7 +133,7 @@
       listShow() {
         let show = false;
         if(this.totalCount > 0) {
-          show = !this.fold;
+          show = !show;
         }
         return show;
       }
@@ -157,7 +157,6 @@
             let y = -(window.innerHeight - rect.top - 22);
             el.style.display = '';// 从display: none到显示
             el.style.transform = `translate3d(0, ${y}px, 0)`;
-            console.log(el.style.transform);
             el.style.webkitTransform = `translate3d(0, ${y}px, 0)`;
             let inner = el.getElementsByClassName('inner-hook')[0];
             inner.style.transform = `translate3d(${x}px, 0, 0)`;
@@ -195,6 +194,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl";
   .shopcart
     position: fixed
     bottom: 0
@@ -284,24 +284,69 @@
           &.enough
             background: #00b43c
             color: #fff
-  .ball-container
-    .ball
-      position: fixed
-      left: 32px
-      bottom: 22px
-      z-index: 200
-      transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
-      .inner
-        width: 16px
-        height: 16px
-        border-radius: 50%
-        background: rgb(0, 160, 220)
-        transition: all 0.4s linear
-  .shopcart-list
-    position: fixed
-    bottom: 611px
-    left: 0
-    width: 100%
-
-
+    .ball-container
+      .ball
+        position: fixed
+        left: 32px
+        bottom: 22px
+        z-index: 200
+        transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
+        .inner
+          width: 16px
+          height: 16px
+          border-radius: 50%
+          background: rgb(0, 160, 220)
+          transition: all 0.4s linear
+    .shopcart-list
+      position: absolute
+      top: 0
+      left: 0
+      z-index: -1
+      width: 100%
+      transform: translate3d(0, -100%, 0)
+      &.fade-enter,&.fade-leave-active
+        transition: all 0.5s
+      &.fade-enter
+        transform: translate3d(0, 0, 0)
+      .list-header
+        padding: 0 18px
+        background: #f3f5f7
+        height: 40px
+        line-height: 40px
+        font-size: 0px
+        line-weight: 200
+        border-1px(rgba(7, 17, 27, 0.1))
+        .title
+          float:left
+          font-size: 12px
+          color: rgb(7, 17, 27)
+        .empty
+          float: right
+          font-size: 14px
+          color: rgb(0, 160, 220)
+      .list-content
+        padding: 0px 18px
+        background: #fff
+        max-height: 217px
+        overflow: hidden
+        .food
+          position: relative
+          padding: 12px 0
+          box-sizing: border-box
+          height: 48px
+          line-height: 24px
+          border-1px(rgba(7, 17, 27, 0.1))
+          .name
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .price
+            position: absolute
+            right: 90px
+            vertical-align: top
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .cartcontrol-wrapper
+            position: absolute
+            right: 0
+            bottom: 0
 </style>
